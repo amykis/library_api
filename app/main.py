9e370ee693dc420f6225d6b600552c3a9f801d71
Aleksey Myakinin <amykis@mail.ru>
2025-05-29 17:12:06 +0300

    edit app\main.py added reader_router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app.api.book import router as book_router
 from app.api.user import router as user_router
+from app.api.reader import router as reader_router
 
 app = FastAPI()
 app.include_router(book_router, prefix="/books", tags=["Books"])
 app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(reader_router, prefix="/readers", tags=["Readers"])
 
 
 @app.get("/")
