d7f1480e54d90e79f33566b177ef7932cd6a0c3d
Aleksey Myakinin <amykis@mail.ru>
2025-05-29 15:11:48 +0300

    import bd

--- app/api/book.py
+++ app/api/book.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schemas.book import BookCreate, BookResponse, BookUpdate
 from app.crud import book as crud_book
-from app.db.database import get_db
+from app.core.database import get_db
 
 router = APIRouter()
 
